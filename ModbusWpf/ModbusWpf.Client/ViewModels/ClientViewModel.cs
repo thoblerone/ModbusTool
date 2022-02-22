fbebfc60ffd1625615958f59a01649a44dbed8e5
blenkers <blenkers@axelsemrau.de>
2022-02-22 12:28:30 +0100

    Add new Client and Server projects

---
+++ ModbusWpf/ModbusWpf.Client/ViewModels/ClientViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ModbusWpf.Common.ViewModels;
+
+namespace ModbusWpf.Client.ViewModels
+{
+    class ClientViewModel : BaseViewModel
+    {
+        public ClientViewModel() : base()
+        {
+        }
+
+        protected override Task InitializeAsync()
+        {
+            MasterOptionsVisibility = Visibility.Visible;
+            SlaveOptionsVisibility = Visibility.Collapsed;
+
+            return base.InitializeAsync();
+        }
+
+    }
+}
---
+++ ModbusWpf/ModbusWpf.Server/ViewModels/ServerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ModbusWpf.Common.ViewModels;
+
+namespace ModbusWpf.Server.ViewModels
+{
+    public class ServerViewModel : BaseViewModel
+    {
+        public ServerViewModel() : base()
+        {
+        }
+
+        protected override Task InitializeAsync()
+        {
+            MasterOptionsVisibility = Visibility.Collapsed;
+            SlaveOptionsVisibility = Visibility.Visible;
+            
+            return base.InitializeAsync();
+        }
+
+    }
+}
