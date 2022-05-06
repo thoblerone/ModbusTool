57c49afac4def703097be832bec0fe3458d515c8
blenkers <blenkers@axelsemrau.de>
2022-05-06 17:15:22 +0200

    Hilfsfunktionen fürs Setzen der Registerwerte

--- ModbusWpf/Modbus.IoC/Interfaces/IRegisterDataService.cs
+++ ModbusWpf/Modbus.IoC/Interfaces/IRegisterDataService.cs
@@ -8,6 +8,14 @@ namespace Modbus.Ioc.Interfaces
     {
         IRegisterDataValue[] RegisterData { get; set; }
         ushort this[int index] { get; set; }
+
+        float GetFloatValue(int index);
+        void SetFloatValue(int index, float value);
+        float GetFloatReverseValue(int index);
+        void SetFloatReverseValue(int index, float value);
+        int GetIntValue(int index);
+        void SetIntValue(int index, int value);
+
     }
 
     public delegate void RegisterValueChangedHandler(ushort oldValue, ushort newValue);
--- ModbusWpf/Modbus.IoC/Services/RegisterDataService.cs
+++ ModbusWpf/Modbus.IoC/Services/RegisterDataService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
 using Modbus.Ioc.Interfaces;
 using Modbus.Ioc.Models;
 
@@ -24,6 +26,86 @@ namespace Modbus.Ioc.Services
         {
             get => RegisterData[index].RegisterValue;
             set => RegisterData[index].RegisterValue = value;
+        }
+
+        public float GetFloatValue(int index)
+        {
+            if (index >= RegisterData.Length - 1)
+                return float.NaN;
+
+            var dataUshort1 = RegisterData[index].RegisterValue;
+            var dataUshort2 = RegisterData[index + 1].RegisterValue;
+
+            var bytes = new byte[4];
+
+            bytes[0] = (byte)(dataUshort1 % 255);
+            bytes[1] = (byte)((dataUshort1 >> 8) % 255);
+            bytes[2] = (byte)(dataUshort2 % 255);
+            bytes[3] = (byte)((dataUshort2 >> 8) % 255);
+
+            return System.BitConverter.ToSingle(bytes, 0);
+        }
+
+        public void SetFloatValue(int index, float value)
+        {
+            if (index >= RegisterData.Length - 1)
+                throw new IndexOutOfRangeException();
+
+            var bytes = BitConverter.GetBytes(value);
+
+            RegisterData[index].RegisterValue = (ushort)((bytes[1] << 8) + bytes[0]);
+            RegisterData[index + 1].RegisterValue = (ushort)((bytes[3] << 8) + bytes[2]);
+        }
+
+        public float GetFloatReverseValue(int index)
+        {
+            if (index >= RegisterData.Length - 1)
+                return float.NaN;
+
+            var dataUshort1 = RegisterData[index].RegisterValue;
+            var dataUshort2 = RegisterData[index + 1].RegisterValue;
+
+            var bytes = new byte[4];
+
+            bytes[2] = (byte)(dataUshort1 % 256);
+            bytes[3] = (byte)((dataUshort1 >> 8) % 256);
+            bytes[0] = (byte)(dataUshort2 % 256);
+            bytes[1] = (byte)((dataUshort2 >> 8) % 256);
+
+            return System.BitConverter.ToSingle(bytes, 0);
+        }
+
+        public void SetFloatReverseValue(int index, float value)
+        {
+            if (index >= RegisterData.Length - 1)
+                throw new IndexOutOfRangeException();
+
+            var bytes = BitConverter.GetBytes(value);
+
+            RegisterData[index].RegisterValue =   (ushort)((bytes[1] << 8) + bytes[0]);
+            RegisterData[index+1].RegisterValue = (ushort)((bytes[2] << 8) + bytes[3]);
+        }
+
+        public int GetIntValue(int index)
+        {
+            if (index >= RegisterData.Length - 1)
+                throw new IndexOutOfRangeException();
+
+            var dataUshort1 = RegisterData[index].RegisterValue;
+            var dataUshort2 = RegisterData[index + 1].RegisterValue;
+
+            return (dataUshort1 << 16) + dataUshort2;
+        }
+
+        public void SetIntValue(int index, int value)
+        {
+            if (index >= RegisterData.Length - 1)
+                throw new IndexOutOfRangeException();
+
+            var bytes = BitConverter.GetBytes(value);
+
+            RegisterData[index].RegisterValue = (ushort)((bytes[1] << 8) + bytes[0]);
+            RegisterData[index + 1].RegisterValue = (ushort)((bytes[2] << 8) + bytes[3]);
         }
     }
 }
