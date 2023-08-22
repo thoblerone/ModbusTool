cfdce2719aa8ea1992bb1946e7e83dcdadee239b
blenkers <blenkers@axelsemrau.de>
2023-08-22 13:33:00 +0200

    Migration to .net Framework 4.8 for ModubsLib, Modbus.IoC & ModbusWpf.Common

--- ModbusWpf/ModbusWpf.Common/Properties/Settings.Designer.cs
+++ ModbusWpf/ModbusWpf.Common/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ModbusWpf.Common.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -169,7 +169,7 @@ namespace ModbusWpf.Common.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("FloatReverse")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Integer")]
         public global::ModbusWpf.Common.Helpers.DisplayFormat DisplayFormat {
             get {
                 return ((global::ModbusWpf.Common.Helpers.DisplayFormat)(this["DisplayFormat"]));
